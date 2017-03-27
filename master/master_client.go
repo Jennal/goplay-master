26d7999465d11600d7fe6bf57477661f8d3c387b
Jennal <jennalcn@gmail.com>
2017-03-27 22:34:27 +0800

    make master client easier to call

--- master/master_client.go
+++ master/master_client.go
@@ -32,28 +32,115 @@ func (self *MasterClient) Add(pack *ServicePack) (stat pkg.Status, err *pkg.Erro
 		if e != nil {
 			stat = pkg.STAT_ERR
 			err = pkg.NewErrorMessage(pkg.STAT_ERR, e.Error())
+			c <- true
 		}
 	})
 
 	return
 }
 
-func (self *MasterClient) Update(pack *ServicePack, succCB func(pkg.Status), failCB func(*pkg.ErrorMessage)) error {
-	return self.Request("master.services.update", pack, succCB, failCB)
+func (self *MasterClient) Update(pack *ServicePack) (stat pkg.Status, err *pkg.ErrorMessage) {
+	aop.Parallel(func(c chan bool) {
+		e := self.Request("master.services.update", pack, func(s pkg.Status) {
+			stat = s
+			err = nil
+			c <- true
+		}, func(e *pkg.ErrorMessage) {
+			stat = pkg.STAT_ERR
+			err = e
+			c <- true
+		})
+
+		if e != nil {
+			stat = pkg.STAT_ERR
+			err = pkg.NewErrorMessage(pkg.STAT_ERR, e.Error())
+			c <- true
+		}
+	})
+
+	return
 }
 
-func (self *MasterClient) GetListByName(name string, succCB func([]ServicePack), failCB func(*pkg.ErrorMessage)) error {
-	return self.Request("master.services.getlistbyname", name, succCB, failCB)
+func (self *MasterClient) GetListByName(name string) (result []ServicePack, err *pkg.ErrorMessage) {
+	aop.Parallel(func(c chan bool) {
+		e := self.Request("master.services.getlistbyname", name, func(s []ServicePack) {
+			result = s
+			err = nil
+			c <- true
+		}, func(e *pkg.ErrorMessage) {
+			result = nil
+			err = e
+			c <- true
+		})
+
+		if e != nil {
+			result = nil
+			err = pkg.NewErrorMessage(pkg.STAT_ERR, e.Error())
+			c <- true
+		}
+	})
+
+	return
 }
 
-func (self *MasterClient) GetListByTags(tags []string, succCB func([]ServicePack), failCB func(*pkg.ErrorMessage)) error {
-	return self.Request("master.services.getlistbytags", tags, succCB, failCB)
+func (self *MasterClient) GetListByTags(tags []string) (result []ServicePack, err *pkg.ErrorMessage) {
+	aop.Parallel(func(c chan bool) {
+		e := self.Request("master.services.getlistbytags", tags, func(s []ServicePack) {
+			result = s
+			err = nil
+			c <- true
+		}, func(e *pkg.ErrorMessage) {
+			result = nil
+			err = e
+			c <- true
+		})
+
+		if e != nil {
+			result = nil
+			err = pkg.NewErrorMessage(pkg.STAT_ERR, e.Error())
+			c <- true
+		}
+	})
+
+	return
 }
 
-func (self *MasterClient) GetByName(name string, succCB func(ServicePack), failCB func(*pkg.ErrorMessage)) error {
-	return self.Request("master.services.getbyname", name, succCB, failCB)
+func (self *MasterClient) GetByName(name string) (result ServicePack, err *pkg.ErrorMessage) {
+	aop.Parallel(func(c chan bool) {
+		e := self.Request("master.services.getbyname", name, func(s ServicePack) {
+			result = s
+			err = nil
+			c <- true
+		}, func(e *pkg.ErrorMessage) {
+			err = e
+			c <- true
+		})
+
+		if e != nil {
+			err = pkg.NewErrorMessage(pkg.STAT_ERR, e.Error())
+			c <- true
+		}
+	})
+
+	return
 }
 
-func (self *MasterClient) GetByTags(tags []string, succCB func(ServicePack), failCB func(*pkg.ErrorMessage)) error {
-	return self.Request("master.services.getbytags", tags, succCB, failCB)
+func (self *MasterClient) GetByTags(tags []string) (result ServicePack, err *pkg.ErrorMessage) {
+	aop.Parallel(func(c chan bool) {
+		e := self.Request("master.services.getbytags", tags, func(s ServicePack) {
+			result = s
+			err = nil
+			c <- true
+		}, func(e *pkg.ErrorMessage) {
+			err = e
+			c <- true
+		})
+
+		if e != nil {
+			err = pkg.NewErrorMessage(pkg.STAT_ERR, e.Error())
+			c <- true
+		}
+	})
+
+	return
 }
--- master/master_test.go
+++ master/master_test.go
@@ -4,8 +4,6 @@ import (
 	"sort"
 	"testing"
 
-	"time"
-
 	"github.com/jennal/goplay/data"
 	"github.com/jennal/goplay/pkg"
 	"github.com/jennal/goplay/service"
@@ -51,310 +49,251 @@ func TestMasterClient(t *testing.T) {
 		ClientCount: 0,
 	}
 
-	callIn := make(map[string]bool)
-
-	client.Add(sp, func(s pkg.Status) {
-		assert.Equal(t, pkg.STAT_OK, s)
-
-		assert.Equal(t, map[int]ServicePack{
-			0: ServicePack{
-				TagContainerImpl: data.TagContainerImpl{
-					Tags: map[string]bool{
-						"Hello": true,
-					},
+	s, err := client.Add(&sp)
+	assert.Equal(t, pkg.STAT_OK, s)
+	assert.Nil(t, err)
+	assert.Equal(t, map[uint32]ServicePack{
+		0: ServicePack{
+			TagContainerImpl: data.TagContainerImpl{
+				Tags: map[string]bool{
+					"Hello": true,
 				},
-				Type:        ST_MASTER,
-				Name:        NAME,
-				IP:          "127.0.0.1",
-				Port:        PORT,
-				ClientCount: 0,
 			},
-		}, hdl.serviceInfos)
-		callIn["add"] = true
-	}, func(err *pkg.ErrorMessage) {
-		assert.True(t, false, "never come to here")
-	})
-	time.Sleep(100 * time.Millisecond)
+			Type:        ST_MASTER,
+			Name:        NAME,
+			IP:          "127.0.0.1",
+			Port:        PORT,
+			ClientCount: 0,
+		},
+	}, hdl.serviceInfos)
+
 	newSp := sp
 	newSp.Tags = map[string]bool{
 		"Hello": true,
 		"World": true,
 	}
-	client1.Add(newSp, func(s pkg.Status) {
-		assert.Equal(t, pkg.STAT_OK, s)
+	s, err = client1.Add(&newSp)
+	assert.Equal(t, pkg.STAT_OK, s)
+	assert.Nil(t, err)
 
-		assert.Equal(t, map[int]ServicePack{
-			0: ServicePack{
-				TagContainerImpl: data.TagContainerImpl{
-					Tags: map[string]bool{
-						"Hello": true,
-					},
+	assert.Equal(t, map[uint32]ServicePack{
+		0: ServicePack{
+			TagContainerImpl: data.TagContainerImpl{
+				Tags: map[string]bool{
+					"Hello": true,
 				},
-				Type:        ST_MASTER,
-				Name:        NAME,
-				IP:          "127.0.0.1",
-				Port:        PORT,
-				ClientCount: 0,
 			},
-			1: ServicePack{
-				TagContainerImpl: data.TagContainerImpl{
-					Tags: map[string]bool{
-						"Hello": true,
-						"World": true,
-					},
+			Type:        ST_MASTER,
+			Name:        NAME,
+			IP:          "127.0.0.1",
+			Port:        PORT,
+			ClientCount: 0,
+		},
+		1: ServicePack{
+			TagContainerImpl: data.TagContainerImpl{
+				Tags: map[string]bool{
+					"Hello": true,
+					"World": true,
 				},
-				Type:        ST_MASTER,
-				Name:        NAME,
-				IP:          "127.0.0.1",
-				Port:        PORT,
-				ClientCount: 0,
 			},
-		}, hdl.serviceInfos)
-		callIn["add1"] = true
-	}, func(err *pkg.ErrorMessage) {
-		assert.True(t, false, "never come to here")
-	})
-	time.Sleep(100 * time.Millisecond)
+			Type:        ST_MASTER,
+			Name:        NAME,
+			IP:          "127.0.0.1",
+			Port:        PORT,
+			ClientCount: 0,
+		},
+	}, hdl.serviceInfos)
+
 	sp.ClientCount = 10
-	client.Update(sp, func(s pkg.Status) {
-		assert.Equal(t, pkg.STAT_OK, s)
+	s, err = client.Update(&sp)
+	assert.Equal(t, pkg.STAT_OK, s)
+	assert.Nil(t, err)
 
-		assert.Equal(t, map[int]ServicePack{
-			0: ServicePack{
-				TagContainerImpl: data.TagContainerImpl{
-					Tags: map[string]bool{
-						"Hello": true,
-					},
+	assert.Equal(t, map[uint32]ServicePack{
+		0: ServicePack{
+			TagContainerImpl: data.TagContainerImpl{
+				Tags: map[string]bool{
+					"Hello": true,
 				},
-				Type:        ST_MASTER,
-				Name:        NAME,
-				IP:          "127.0.0.1",
-				Port:        PORT,
-				ClientCount: 10,
 			},
-			1: ServicePack{
-				TagContainerImpl: data.TagContainerImpl{
-					Tags: map[string]bool{
-						"Hello": true,
-						"World": true,
-					},
+			Type:        ST_MASTER,
+			Name:        NAME,
+			IP:          "127.0.0.1",
+			Port:        PORT,
+			ClientCount: 10,
+		},
+		1: ServicePack{
+			TagContainerImpl: data.TagContainerImpl{
+				Tags: map[string]bool{
+					"Hello": true,
+					"World": true,
 				},
-				Type:        ST_MASTER,
-				Name:        NAME,
-				IP:          "127.0.0.1",
-				Port:        PORT,
-				ClientCount: 0,
 			},
-		}, hdl.serviceInfos)
-		callIn["update"] = true
-	}, func(err *pkg.ErrorMessage) {
-		assert.True(t, false, "never come to here")
-	})
-	time.Sleep(100 * time.Millisecond)
-	client.GetListByName("test", func(list []ServicePack) {
-		assert.True(t, false, "never come to here")
-	}, func(err *pkg.ErrorMessage) {
-		assert.Equal(t, pkg.NewErrorMessage(pkg.STAT_ERR_EMPTY_RESULT, "no results"), err)
-		callIn["GetListByName"] = true
-	})
+			Type:        ST_MASTER,
+			Name:        NAME,
+			IP:          "127.0.0.1",
+			Port:        PORT,
+			ClientCount: 0,
+		},
+	}, hdl.serviceInfos)
 
-	client.GetListByName(NAME, func(list []ServicePack) {
-		sortList(list)
+	list, err := client.GetListByName("test")
+	assert.Nil(t, list)
+	assert.Equal(t, pkg.NewErrorMessage(pkg.STAT_ERR_EMPTY_RESULT, "no results"), err)
 
-		assert.Equal(t, []ServicePack{
-			ServicePack{
-				TagContainerImpl: data.TagContainerImpl{
-					Tags: map[string]bool{
-						"Hello": true,
-					},
-				},
-				Type:        ST_MASTER,
-				Name:        NAME,
-				IP:          "127.0.0.1",
-				Port:        PORT,
-				ClientCount: 10,
-			},
-			ServicePack{
-				TagContainerImpl: data.TagContainerImpl{
-					Tags: map[string]bool{
-						"Hello": true,
-						"World": true,
-					},
-				},
-				Type:        ST_MASTER,
-				Name:        NAME,
-				IP:          "127.0.0.1",
-				Port:        PORT,
-				ClientCount: 0,
-			},
-		}, list)
-		callIn["GetListByName1"] = true
-	}, func(err *pkg.ErrorMessage) {
-		assert.True(t, false, "never come to here")
-	})
+	list, err = client.GetListByName(NAME)
+	assert.Nil(t, err)
 
-	client.GetListByTags([]string{
+	sortList(list)
+	assert.Equal(t, []ServicePack{
+		ServicePack{
+			TagContainerImpl: data.TagContainerImpl{
+				Tags: map[string]bool{
+					"Hello": true,
+				},
+			},
+			Type:        ST_MASTER,
+			Name:        NAME,
+			IP:          "127.0.0.1",
+			Port:        PORT,
+			ClientCount: 10,
+		},
+		ServicePack{
+			TagContainerImpl: data.TagContainerImpl{
+				Tags: map[string]bool{
+					"Hello": true,
+					"World": true,
+				},
+			},
+			Type:        ST_MASTER,
+			Name:        NAME,
+			IP:          "127.0.0.1",
+			Port:        PORT,
+			ClientCount: 0,
+		},
+	}, list)
+
+	list, err = client.GetListByTags([]string{
 		"Hello",
-	}, func(list []ServicePack) {
-		sortList(list)
-
-		assert.Equal(t, []ServicePack{
-			ServicePack{
-				TagContainerImpl: data.TagContainerImpl{
-					Tags: map[string]bool{
-						"Hello": true,
-					},
-				},
-				Type:        ST_MASTER,
-				Name:        NAME,
-				IP:          "127.0.0.1",
-				Port:        PORT,
-				ClientCount: 10,
-			},
-			ServicePack{
-				TagContainerImpl: data.TagContainerImpl{
-					Tags: map[string]bool{
-						"Hello": true,
-						"World": true,
-					},
-				},
-				Type:        ST_MASTER,
-				Name:        NAME,
-				IP:          "127.0.0.1",
-				Port:        PORT,
-				ClientCount: 0,
-			},
-		}, list)
-		callIn["GetListByTags"] = true
-	}, func(err *pkg.ErrorMessage) {
-		assert.True(t, false, "never come to here")
 	})
+	assert.Nil(t, err)
 
-	client.GetListByTags([]string{
+	sortList(list)
+	assert.Equal(t, []ServicePack{
+		ServicePack{
+			TagContainerImpl: data.TagContainerImpl{
+				Tags: map[string]bool{
+					"Hello": true,
+				},
+			},
+			Type:        ST_MASTER,
+			Name:        NAME,
+			IP:          "127.0.0.1",
+			Port:        PORT,
+			ClientCount: 10,
+		},
+		ServicePack{
+			TagContainerImpl: data.TagContainerImpl{
+				Tags: map[string]bool{
+					"Hello": true,
+					"World": true,
+				},
+			},
+			Type:        ST_MASTER,
+			Name:        NAME,
+			IP:          "127.0.0.1",
+			Port:        PORT,
+			ClientCount: 0,
+		},
+	}, list)
+
+	list, err = client.GetListByTags([]string{
 		"Hello", "World",
-	}, func(list []ServicePack) {
-		sortList(list)
-
-		assert.Equal(t, []ServicePack{
-			ServicePack{
-				TagContainerImpl: data.TagContainerImpl{
-					Tags: map[string]bool{
-						"Hello": true,
-						"World": true,
-					},
-				},
-				Type:        ST_MASTER,
-				Name:        NAME,
-				IP:          "127.0.0.1",
-				Port:        PORT,
-				ClientCount: 0,
-			},
-		}, list)
-		callIn["GetListByTags1"] = true
-	}, func(err *pkg.ErrorMessage) {
-		assert.True(t, false, "never come to here")
 	})
+	assert.Nil(t, err)
 
-	client.GetListByTags([]string{
+	sortList(list)
+	assert.Equal(t, []ServicePack{
+		ServicePack{
+			TagContainerImpl: data.TagContainerImpl{
+				Tags: map[string]bool{
+					"Hello": true,
+					"World": true,
+				},
+			},
+			Type:        ST_MASTER,
+			Name:        NAME,
+			IP:          "127.0.0.1",
+			Port:        PORT,
+			ClientCount: 0,
+		},
+	}, list)
+
+	list, err = client.GetListByTags([]string{
 		"Hello", "world",
-	}, func(list []ServicePack) {
-		assert.True(t, false, "never come to here")
-	}, func(err *pkg.ErrorMessage) {
-		assert.Equal(t, pkg.NewErrorMessage(pkg.STAT_ERR_EMPTY_RESULT, "no results"), err)
-		callIn["GetListByTags2"] = true
 	})
+	assert.Nil(t, list)
+	assert.Equal(t, pkg.NewErrorMessage(pkg.STAT_ERR_EMPTY_RESULT, "no results"), err)
 
-	client.GetByName(NAME, func(sp ServicePack) {
-		assert.Equal(t, ServicePack{
-			TagContainerImpl: data.TagContainerImpl{
-				Tags: map[string]bool{
-					"Hello": true,
-					"World": true,
-				},
+	result, err := client.GetByName(NAME)
+	assert.Equal(t, ServicePack{
+		TagContainerImpl: data.TagContainerImpl{
+			Tags: map[string]bool{
+				"Hello": true,
+				"World": true,
 			},
-			Type:        ST_MASTER,
-			Name:        NAME,
-			IP:          "127.0.0.1",
-			Port:        PORT,
-			ClientCount: 0,
-		}, sp)
-		callIn["GetByName"] = true
-	}, func(err *pkg.ErrorMessage) {
-		assert.True(t, false, "never come to here")
-	})
+		},
+		Type:        ST_MASTER,
+		Name:        NAME,
+		IP:          "127.0.0.1",
+		Port:        PORT,
+		ClientCount: 0,
+	}, result)
+	assert.Nil(t, err)
 
-	client.GetByName("none", func(sp ServicePack) {
-		assert.True(t, false, "never come to here")
-	}, func(err *pkg.ErrorMessage) {
-		assert.Equal(t, pkg.NewErrorMessage(pkg.STAT_ERR_EMPTY_RESULT, "no results"), err)
-		callIn["GetByName1"] = true
-	})
+	result, err = client.GetByName("none")
+	assert.Equal(t, ServicePack{}, result)
+	assert.Equal(t, pkg.NewErrorMessage(pkg.STAT_ERR_EMPTY_RESULT, "no results"), err)
 
-	client.GetByTags([]string{
+	result, err = client.GetByTags([]string{
 		"Hello",
-	}, func(sp ServicePack) {
-		assert.Equal(t, ServicePack{
-			TagContainerImpl: data.TagContainerImpl{
-				Tags: map[string]bool{
-					"Hello": true,
-					"World": true,
-				},
-			},
-			Type:        ST_MASTER,
-			Name:        NAME,
-			IP:          "127.0.0.1",
-			Port:        PORT,
-			ClientCount: 0,
-		}, sp)
-		callIn["GetByTags"] = true
-	}, func(err *pkg.ErrorMessage) {
-		assert.True(t, false, "never come to here")
 	})
+	assert.Equal(t, ServicePack{
+		TagContainerImpl: data.TagContainerImpl{
+			Tags: map[string]bool{
+				"Hello": true,
+				"World": true,
+			},
+		},
+		Type:        ST_MASTER,
+		Name:        NAME,
+		IP:          "127.0.0.1",
+		Port:        PORT,
+		ClientCount: 0,
+	}, result)
+	assert.Nil(t, err)
 
-	client.GetByTags([]string{
+	result, err = client.GetByTags([]string{
 		"Hello", "World",
-	}, func(sp ServicePack) {
-		assert.Equal(t, ServicePack{
-			TagContainerImpl: data.TagContainerImpl{
-				Tags: map[string]bool{
-					"Hello": true,
-					"World": true,
-				},
+	})
+	assert.Equal(t, ServicePack{
+		TagContainerImpl: data.TagContainerImpl{
+			Tags: map[string]bool{
+				"Hello": true,
+				"World": true,
 			},
-			Type:        ST_MASTER,
-			Name:        NAME,
-			IP:          "127.0.0.1",
-			Port:        PORT,
-			ClientCount: 0,
-		}, sp)
-		callIn["GetByTags1"] = true
-	}, func(err *pkg.ErrorMessage) {
-		assert.True(t, false, "never come to here")
-	})
+		},
+		Type:        ST_MASTER,
+		Name:        NAME,
+		IP:          "127.0.0.1",
+		Port:        PORT,
+		ClientCount: 0,
+	}, result)
+	assert.Nil(t, err)
 
-	client.GetByTags([]string{
+	result, err = client.GetByTags([]string{
 		"Hello", "not exists tag",
-	}, func(sp ServicePack) {
-		assert.True(t, false, "never come to here")
-	}, func(err *pkg.ErrorMessage) {
-		assert.Equal(t, pkg.NewErrorMessage(pkg.STAT_ERR_EMPTY_RESULT, "no results"), err)
-		callIn["GetByTags2"] = true
 	})
-
-	time.Sleep(100 * time.Millisecond)
-	assert.Equal(t, map[string]bool{
-		"add":            true,
-		"add1":           true,
-		"update":         true,
-		"GetListByName":  true,
-		"GetListByName1": true,
-		"GetListByTags":  true,
-		"GetListByTags1": true,
-		"GetListByTags2": true,
-		"GetByName":      true,
-		"GetByName1":     true,
-		"GetByTags":      true,
-		"GetByTags1":     true,
-		"GetByTags2":     true,
-	}, callIn)
+	assert.Equal(t, ServicePack{}, result)
+	assert.Equal(t, pkg.NewErrorMessage(pkg.STAT_ERR_EMPTY_RESULT, "no results"), err)
 }
