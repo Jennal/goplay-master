75dafe5a3d278bdfa901a99ee5195efd628b9e56
jennal <jennalcn@gmail.com>
2017-03-27 18:53:03 +0800

    simpfy call to Add

--- master/master_client.go
+++ master/master_client.go
@@ -1,6 +1,7 @@
 package master
 
 import (
+	"github.com/jennal/goplay/aop"
 	"github.com/jennal/goplay/pkg"
 	"github.com/jennal/goplay/service"
 	"github.com/jennal/goplay/transfer"
@@ -16,8 +17,25 @@ func NewMasterClient(cli transfer.IClient) *MasterClient {
 	}
 }
 
-func (self *MasterClient) Add(pack *ServicePack, succCB func(pkg.Status), failCB func(*pkg.ErrorMessage)) error {
-	return self.Request("master.services.add", pack, succCB, failCB)
+func (self *MasterClient) Add(pack *ServicePack) (stat pkg.Status, err *pkg.ErrorMessage) {
+	aop.Parallel(func(c chan bool) {
+		e := self.Request("master.services.add", pack, func(st pkg.Status) {
+			stat = st
+			err = nil
+			c <- true
+		}, func(e *pkg.ErrorMessage) {
+			stat = pkg.STAT_ERR
+			err = e
+			c <- true
+		})
+
+		if e != nil {
+			stat = pkg.STAT_ERR
+			err = pkg.NewErrorMessage(pkg.STAT_ERR, e.Error())
+		}
+	})
+
+	return
 }
 
 func (self *MasterClient) Update(pack *ServicePack, succCB func(pkg.Status), failCB func(*pkg.ErrorMessage)) error {
